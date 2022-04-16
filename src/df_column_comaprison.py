9e72dff7077264c4e6a3e052046c9656100fc27f
sghub7 <shoukatghouse.as@gmail.com>
2022-04-16 17:55:41 +0530

    Added samples

--- src/df_column_comaprison.py
+++ src/df_column_comaprison.py
@@ -161,101 +161,14 @@ df = spark.read.parquet("compare_details")
 df.show()
 """
 sample output - 
-[{'segmentColumns': 'SU___S1', 'mismatches': 2}, {'segmentColumns': 'SU___S2', 'mismatches': 2}, {'segmentColumns': 'DU___D1', 'mismatches': 0}, {'segmentColumns': 'DU___D2', 'mismatches': 1}]
++---+-----+-------------+-------+--------+--------+-------------+------------+-----------------------+------------------+------------------+-----------------------+-------------------+---------------+
+| id| name|applied_limit|address|segment1|segment2|masterSegment|xx_target_id|xx_target_applied_limit|xx_target_segment1|xx_target_segment2|xx_target_masterSegment| mismatched_columns|numColsMismatch|
++---+-----+-------------+-------+--------+--------+-------------+------------+-----------------------+------------------+------------------+-----------------------+-------------------+---------------+
+| 11|User1|        10000|     A1|      SU|      S1|      SU___S1|           1|                  10000|                SU|                S1|                SU___S1|               [id]|              1|
+|  2|User2|        30000|     A2|      SU|      S1|      SU___S1|           2|                  20000|                SU|                S1|                SU___S1|    [applied_limit]|              1|
+| 50|User5|        15000|     A5|      SU|      S2|      SU___S2|          68|                  17000|                SU|                S2|                SU___S2|[id, applied_limit]|              2|
+| 40|User4|         9000|     A4|      DU|      D2|      DU___D2|           4|                   9000|                DU|                D2|                DU___D2|               [id]|              1|
+| 30|User3|        20000|     A3|      DU|      D1|      DU___D1|          30|                  20000|                DU|                D1|                DU___D1|                 []|              0|
++---+-----+-------------+-------+--------+--------+-------------+------------+-----------------------+------------------+------------------+-----------------------+-------------------+---------------+
 
 """
-
-
-
-
-# """
-#
-# print("**********here")
-# """
-# Program assumes the segments are common across src and target. If not, throw an exception
-# """
-
-# Below loop iterates for each target.
-# Compares teh stats at segment level
-# Appends the stats in a list
-# Finally creates a dictionary and writes the final status for all segments
-# """
-# masterStatus =[]
-# for index,target in enumerate(trgArray):
-#     print("***Target Dataframes***")
-#     target.show()
-#
-#     #Prepending columns with "target_" for differentiating taget and source columns
-#     target = target.selectExpr([colName+' as xx_target_' + colName for colName in target.columns]) \
-#         .withColumnRenamed("xx_target_summary","summary")
-#     target.show()
-#     target.printSchema()
-#
-#
-#     """
-#     # ## Mismatched list. Comapare each df in source and target df arrays
-#     ## Assumption - Src and target have same set of segments.. Handled by line #73
-#     ##
-#     """
-#
-#     mismatches = [when( ( (target["xx_target_"+c]) > threshold * srcArray[index][c] + srcArray[index][c] ) | ( (target["xx_target_"+c]) < srcArray[index][c]  - threshold * srcArray[index][c]  )  , lit(c)).otherwise("") for c in srcArray[index].columns if c != 'summary']
-#
-#     # ## Create a new col with name mismatched_columns to store the list of mismatched column
-#     ## summary is the column name on which teh dfs will be joind to compare
-#     select_expr =[
-#
-#         *[srcArray[index][c] for c in srcArray[index].columns ],
-#         *[target[c] for c in target.columns if c != 'summary'],
-#         array_remove(array(*mismatches), "").alias("mismatched_columns")#Removes the extra "" record intriduced if not mismatch
-#     ]
-#
-#     src = srcArray[index]
-#
-#     src.show()
-#     compareDF= src.join(target,'summary','inner').select(*select_expr)
-#     compareDF.show()
-#     compareDict  = compareDF.toPandas().to_dict() ## Collecting to driver to store results as dict
-#     print(compareDict["mismatched_columns"])
-#     result="Pass"
-#     for k,v in compareDict["mismatched_columns"].items():
-#         if len(v) > 0:
-#             result="Fail"
-#             break
-#     masterStatus.append({"segment_index":index,"segment_result":result,"details":compareDict})
-#
-# print(masterStatus)
-# ## Render Status Data in readable format
-# for i in masterStatus:
-#     print(i["details"])
-#     import pandas as pd
-#     pd.set_option('display.max_rows', 500)
-#     pd.set_option('display.max_columns', 500)
-#     pd.set_option('display.width', 1000)
-#     print(pd.DataFrame(i["details"]).head())
-#
-# """
-# Sample output -
-# [{'segment_index': 0, 'segment_result': 'Fail', 'details': {'summary': {0: 'count', 1: 'mean', 2: 'stddev', 3: 'min', 4: 'max'}, 'id': {0: '2', 1: '10.5', 2: '13.435028842544403', 3: '1', 4: '20'}, 'name': {0: '2', 1: None, 2: None, 3: 'User1', 4: 'User2'}, 'segment1': {0: '2', 1: None, 2: None, 3: 'SU', 4: 'SU'}, 'segment2': {0: '2', 1: None, 2: None, 3: 'S1', 4: 'S1'}, 'xx_target_id': {0: '2', 1: '1.5', 2: '0.7071067811865476', 3: '1', 4: '2'}, 'xx_target_name': {0: '2', 1: None, 2: None, 3: 'User1', 4: 'User2'}, 'xx_target_segment1': {0: '2', 1: None, 2: None, 3: 'SU', 4: 'SU'}, 'xx_target_segment2': {0: '2', 1: None, 2: None, 3: 'S1', 4: 'S1'}, 'mismatched_columns': {0: [], 1: ['id'], 2: ['id'], 3: [], 4: ['id']}}}, {'segment_index': 1, 'segment_result': 'Pass', 'details': {'summary': {0: 'count', 1: 'mean', 2: 'stddev', 3: 'min', 4: 'max'}, 'id': {0: '1', 1: '50.0', 2: None, 3: '50', 4: '50'}, 'name': {0: '1', 1: None, 2: None, 3: 'User5', 4: 'User5'}, 'segment1': {0: '1', 1: None, 2: None, 3: 'SU', 4: 'SU'}, 'segment2': {0: '1', 1: None, 2: None, 3: 'S2', 4: 'S2'}, 'xx_target_id': {0: '1', 1: '50.0', 2: None, 3: '50', 4: '50'}, 'xx_target_name': {0: '1', 1: None, 2: None, 3: 'User5', 4: 'User5'}, 'xx_target_segment1': {0: '1', 1: None, 2: None, 3: 'SU', 4: 'SU'}, 'xx_target_segment2': {0: '1', 1: None, 2: None, 3: 'S2', 4: 'S2'}, 'mismatched_columns': {0: [], 1: [], 2: [], 3: [], 4: []}}}, {'segment_index': 2, 'segment_result': 'Pass', 'details': {'summary': {0: 'count', 1: 'mean', 2: 'stddev', 3: 'min', 4: 'max'}, 'id': {0: '1', 1: '30.0', 2: None, 3: '30', 4: '30'}, 'name': {0: '1', 1: None, 2: None, 3: 'User3', 4: 'User3'}, 'segment1': {0: '1', 1: None, 2: None, 3: 'DU', 4: 'DU'}, 'segment2': {0: '1', 1: None, 2: None, 3: 'D1', 4: 'D1'}, 'xx_target_id': {0: '1', 1: '30.0', 2: None, 3: '30', 4: '30'}, 'xx_target_name': {0: '1', 1: None, 2: None, 3: 'User3', 4: 'User3'}, 'xx_target_segment1': {0: '1', 1: None, 2: None, 3: 'DU', 4: 'DU'}, 'xx_target_segment2': {0: '1', 1: None, 2: None, 3: 'D1', 4: 'D1'}, 'mismatched_columns': {0: [], 1: [], 2: [], 3: [], 4: []}}}, {'segment_index': 3, 'segment_result': 'Fail', 'details': {'summary': {0: 'count', 1: 'mean', 2: 'stddev', 3: 'min', 4: 'max'}, 'id': {0: '1', 1: '40.0', 2: None, 3: '40', 4: '40'}, 'name': {0: '1', 1: None, 2: None, 3: 'User4', 4: 'User4'}, 'segment1': {0: '1', 1: None, 2: None, 3: 'DU', 4: 'DU'}, 'segment2': {0: '1', 1: None, 2: None, 3: 'D2', 4: 'D2'}, 'xx_target_id': {0: '1', 1: '4.0', 2: None, 3: '4', 4: '4'}, 'xx_target_name': {0: '1', 1: None, 2: None, 3: 'User4', 4: 'User4'}, 'xx_target_segment1': {0: '1', 1: None, 2: None, 3: 'DU', 4: 'DU'}, 'xx_target_segment2': {0: '1', 1: None, 2: None, 3: 'D2', 4: 'D2'}, 'mismatched_columns': {0: [], 1: ['id'], 2: [], 3: ['id'], 4: ['id']}}}]
-#
-# """
-#
-#
-# # ## Creata a new col with name mismatched_columns to store the list of mismatched column
-# # ## summary is the column name on which teh dfs will be joind to compare
-# # select_expr =[
-# #
-# #     *[desc[c] for c in desc.columns ],
-# #     *[desc1[c] for c in desc1.columns if c != 'summary'],
-# #     array_remove(array(*mismatches), "").alias("mismatched_columns")
-# # ]
-# # compareDF= desc.join(desc1,'summary','inner').select(*select_expr)
-# # compareDF.show()
-# # compareDict  = compareDF.toPandas().to_dict() ## Collecting to driver to store results as dict
-# # print(compareDict["mismatched_columns"])
-# # result="Pass"
-# # for k,v in compareDict["mismatched_columns"].items():
-# #     if len(v) > 0:
-# #         result="Fail"
-# #         break
-# # print(result)
-# # print(compareDict)
